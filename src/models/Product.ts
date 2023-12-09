a1bb31905dc087aa77d9b2abb3f2c067fc8ce408
DanielBarreto01 <125820921+DanielBarreto01@users.noreply.github.com>
2023-12-09 12:29:23 -0500

    Refactor product model and controller

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,24 +1,24 @@
 import { Document, Schema, Model, model } from 'mongoose';
 
 interface IProduct extends Document {
-  productModel: string;
-  brand: string;
-  stock: boolean;
-  units: number;
-  price: number;
-  special_price: number;
+  name: string;
+  brand: Schema.Types.ObjectId;
+  basePrice: number;
+  specialPrice: number;
+  inStock: boolean;
+  quantity: number;
 }
 
-const productSchema = new Schema<IProduct>({
-  productModel: String,
-  brand: String,
-  stock: { type: Boolean, default: true },
-  units: { type: Number, default: 0 },
-  price: Number,
-  special_price: Number,
+const ProductSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  brand: { type: Schema.Types.ObjectId, ref: 'Brand' },
+  basePrice: { type: Number, required: true },
+  specialPrice: { type: Number, required: true },
+  inStock: { type: Boolean, default: true },
+  quantity: { type: Number, default: 0 },
 });
 
-const Product: Model<IProduct> = model<IProduct>('Product', productSchema);
+const Product: Model<IProduct> = model<IProduct>('Product', ProductSchema);
 
 export { Product, IProduct };
 
